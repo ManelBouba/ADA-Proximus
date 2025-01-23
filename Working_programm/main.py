fe6615eded076705f687d774be6f78763de8f8b6
kevin87ops <Kevin.jamotte@gmail.com>
2025-01-23 11:30:49 +0100

    generate_email_content takes one more argument: language, added disabled warnings urrllib3 on main, updated dummy taget list with language, limited the 'for loop' to 1 target

--- Working_programm/main.py
+++ Working_programm/main.py
@@ -2,20 +2,26 @@ import Gophish_runner, landing_page, email_generator
 import json
 import time
 import pandas as pd
+import urllib3
+urllib3.disable_warnings()
+
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-targets = [{'name':'Sofie', 'last_name':'Parys', 'position':'Sales & Segment Strategy Manager', 'email':config["email_address"]}, 
-           {'name':'Saransh', 'last_name':'Goyal', 'position':'Strategy', 'email':config["email_address"]},
-           {'name':'Kristiaan', 'last_name':'Vangeel', 'position':'Manager Network Deployment', 'email':config["email_address"]},
-           {'name':'Cindy Joly', 'last_name':' Boyé ', 'position':'IT service delivery manager', 'email':config["email_address"]},
-           {'name':'Zunaira', 'last_name':'Zahid', 'position':'Assistant Project Manager', 'email':config["email_address"]},
-           {'name':'Carl', 'last_name':'Ringoet', 'position':'Product Owner', 'email':config["email_address"]},
-           {'name':'Tom', 'last_name':'Logghe', 'position':'Team Manager eOperations', 'email':config["email_address"]},
-           {'name':'Laetitia', 'last_name':'Bastin', 'position':'Account Manager', 'email':config["email_address"]},
-           {'name':'Michel', 'last_name':'Simonart', 'position':'Head of Digital Ecosystems', 'email':config["email_address"]},
-           {'name':'GEOFFREY', 'last_name':'HARING', 'position':'Service Delivery Manager', 'email':config["email_address"]}]
+targets = [
+    {'name': 'Wout', 'last_name': 'Van Aert', 'position': 'Contact Center Agent', 'email': 'wout.vanaert.visma@proton.me', 'language': 'french'},
+    {'name': 'Remco', 'last_name': 'Evenepoel', 'position': 'Customer Service Specialist', 'email': 'remco.evenepoel.soudal@proton.me' , 'language': 'french'},
+    {'name': 'Tadej', 'last_name': 'Pogacar', 'position': 'Sales Advisor', 'email': 'tadej.pogacar.uae@proton.me', 'language': 'dutch'},
+    {'name': 'Arnaud', 'last_name': 'De Lie', 'position': 'B2B Sales Representative', 'email': 'arnaud.de.lie.lotto@proton.me', 'language': 'dutch'},
+    {'name': 'Mathieu', 'last_name': 'Van Der Poel', 'position': 'Senior Legal Advisor', 'email': 'mathieu.vanderpoel.alpecin@proton.me', 'language': 'french'},
+    {'name': 'Julian', 'last_name': 'Alaphilippe', 'position': 'Payroll Officer', 'email': 'julian.alaphilippe.tudor@proton.me', 'language': 'english'},
+    {'name': 'Tom', 'last_name': 'Pidcock', 'position': 'Compensation & Benefits Specialist', 'email': 'tom.pidcock.q365@proton.me', 'language': 'english'},
+    {'name': "Ben", 'last_name': "O'Connor", 'position': 'Finance Business Partner', 'email': 'ben.oconnor.decathlon@proton.me', 'language': 'french'},
+    {'name': 'Jonas', 'last_name': 'Vingegaard', 'position': 'Finance Specialist', 'email': 'jonas.vinegegaard.visma@proton.me', 'language': 'english'},
+    {'name': 'Victor', 'last_name': 'Campenaerts', 'position': 'Technical Dispatching', 'email': 'victor.campenaerts@proton.me', 'language': 'english'}
+]
+
 
 def main():
     Gophish_Instance = Gophish_runner.Gophish_Runner(config)
@@ -28,8 +34,8 @@ def main():
     Gophish_Instance.create_sending_profile('test')
     Gophish_Instance.create_landing_page('test', landing_page.generate_landing_page_html())
 
-    for target in targets:
-        html_email, subject = email_generator.generate_email_content(config["GEMINI_API_KEY"], target["name"], target['last_name'], target['position'])
+    for target in targets [:1]:
+        html_email, subject = email_generator.generate_email_content(config["GEMINI_API_KEY"], target["name"], target['last_name'], target['position'], target['language'])
         Gophish_Instance.create_email_template(f"{target['name']} {target['last_name']} {subject}", subject, html_email)
         Gophish_Instance.create_group(f"{target['name']} {target['last_name']}", target["name"], target['last_name'], target['email'], target['position'])
         Gophish_Instance.create_campaign(f"{target['name']} {target['last_name']} {subject}", f"{target['name']} {target['last_name']} {subject}", 'test', 'test', f"{target['name']} {target['last_name']}")
